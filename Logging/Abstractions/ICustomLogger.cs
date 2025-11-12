73a1933f1707e7566cd0064f23abecd9c7c4c82a
Iru <irunil@raycynix.com>
2025-11-13 02:08:33 +0300

    Completing refactor

---
+++ Logging/Abstractions/ICustomLogger.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Logging.Abstractions
+{
+    /// <summary>
+    /// Represents a Raycynix logger interface that extends the standard <see cref="ILogger{TCategoryName}"/>.
+    /// </summary>
+    /// <typeparam name="T">The logging category type.</typeparam>
+    public interface ICustomLogger<T> : ILogger<T>
+    {
+        // TODO: custom logging contracts or tracing integration.
+    }
+}
--- Logging/ICustomLogger.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.Extensions.Logging;
-
-namespace Extensions.Logging
-{
-    public interface ICustomLogger<T> : ILogger<T>
-    {
-    }
-}
