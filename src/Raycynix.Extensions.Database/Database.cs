204fc85f8d605fb45b11d0b8591219cd8bdb397d
Iru <irunil@raycynix.com>
2026-02-25 01:49:20 +0300

    Fix

---
+++ src/Raycynix.Extensions.Database/Database.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Raycynix.Extensions.Database;
+
+public static class Database
+{
+    public static IServiceCollection AddRaycynixDatabase(this IServiceCollection services)
+    {
+        return services;
+    }
+}
