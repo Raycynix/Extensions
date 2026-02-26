127e8f081d8d3b53591aa14ca07754260c52a243
Iru <irunil@raycynix.com>
2026-02-26 17:39:41 +0300

    Merge resolve
    Signed-off-by: Iru <irunil@raycynix.com>

--- Database/Abstractions/IDatabaseInitializer.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Raycynix.Extensions.Database.Abstractions;
-
-public interface IDatabaseInitializer
-{
-    Task InitializeAsync(CancellationToken cancellationToken = default);
-    bool IsReady { get; }
-}
