13b31b54562067261323ccade1c92d42b26a4991
Iru <irunil@raycynix.com>
2026-03-14 15:34:51 +0300

    Fix documentation

--- src/Raycynix.Extensions.Exceptions/Options/ExceptionMapperOptions.cs
+++ src/Raycynix.Extensions.Exceptions/Options/ExceptionMapperOptions.cs
@@ -14,8 +14,25 @@ namespace Raycynix.Extensions.Exceptions.Options;
 /// </remarks>
 public class ExceptionMapperOptions
 {
+    /// <summary>
+    /// Gets the collection of mappings that associate exception types with functions
+    /// for transforming them into <see cref="RaycynixException"/> instances.
+    /// </summary>
+    /// <remarks>
+    /// This property stores a dictionary where the key represents the type of the exception
+    /// to be mapped, and the value is a function defining how to transform an exception
+    /// to that type into a <see cref="RaycynixException"/>. These mappings are utilized
+    /// by the <see cref="IExceptionMapper"/> to customize exception handling behavior.
+    /// </remarks>
     public Dictionary<Type, Func<Exception, RaycynixException>> Mappings { get; } = new();
 
-    public void Map<TException>(Func<TException, RaycynixException> mapper) where TException : Exception 
+    /// <summary>
+    /// Registers a mapping for a specific exception type to transform it into a <see cref="RaycynixException"/> instance.
+    /// </summary>
+    /// <typeparam name="TException">The type of the exception to map. Must inherit from <see cref="Exception"/>.</typeparam>
+    /// <param name="mapper">
+    /// A function that converts an exception of type <typeparamref name="TException"/> into an instance of <see cref="RaycynixException"/>.
+    /// </param>
+    public void Map<TException>(Func<TException, RaycynixException> mapper) where TException : Exception
         => Mappings[typeof(TException)] = ex => mapper((TException)ex);
 }
